845eaf595801fb800f1a48bfd0527188a5ef8ac1
Winstonshih <Winstonshih@github.com>
2026-09-24 01:33:11 +0000

    Test run: 0/4 passed, 4 failed, 0 skipped - 2026-09-24T01:33:11.815Z

--- src/main/EventListener.js
+++ src/main/EventListener.js
@@ -33,10 +33,17 @@ reset.addEventListener("click", resetText);
  * updateTextOnEitherButtonClick should fire.
  */
 
-//code here
+button1 = document.querySelector('button1');
 
-
-
+button1.addEventListener('click', () => {
+    updateTextOnButton1Click();
+    updateTextOnEitherButtonClick();
+});
+button2 = document.querySelector('button2');
+button2.addEventListener('click', () => {
+    updateTextOnButton2Click();
+    updateTextOnEitherButtonClick();
+});
 
 
 /**
